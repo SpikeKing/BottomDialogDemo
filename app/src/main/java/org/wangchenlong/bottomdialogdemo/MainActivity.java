c319f3d2168d94370d98a1168d55bd4b3dd68d1d
spike <morndragon@126.com>
2018-01-03 17:27:30 +0800

    update

--- app/src/main/java/org/wangchenlong/bottomdialogdemo/MainActivity.java
+++ app/src/main/java/org/wangchenlong/bottomdialogdemo/MainActivity.java
@@ -6,18 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * 显示底部Dialog
-     *
-     * @param view 视图
-     */
+    // 显示底部Dialog
     public void showBottomDialog(View view) {
         FragmentManager fm = getSupportFragmentManager();
         BottomDialogFragment editNameDialog = new BottomDialogFragment();
